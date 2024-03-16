d20de05cdbcf6c7e890bdc1db2a600f41233398b
Rizwan-ReNote <rizwanm@renote.ai>
2024-03-16 11:04:13 +0530

    setting up of ports

--- app.py
+++ app.py
@@ -171,4 +171,4 @@ def entry_point_for_args(args):
         return entry_point(Path(root), args)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', )
+    app.run(debug=True,host='0.0.0.0', port=5050)
